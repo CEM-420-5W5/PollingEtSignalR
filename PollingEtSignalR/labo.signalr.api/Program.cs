535d247e969b3194404473caf9d654db1d73b92f
Mathieu Briau <mathieu.briau@gmail.com>
2026-08-12 16:46:11 -0400

    solution pour SignalR

--- PollingEtSignalR/labo.signalr.api/Program.cs
+++ PollingEtSignalR/labo.signalr.api/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using labo.signalr.api.Data;
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Hubs;
+using Microsoft.EntityFrameworkCore;
+using labo.signalr.api.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // TODO: SignalR: Ajouter SignalR
+builder.Services.AddSignalR();
 
 builder.Services.AddCors(options =>
 {
@@ -57,6 +60,7 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 // TODO: SignalR: Ajouter la route vers le Hub
+app.MapHub<TaskHub>("/tasks");
 
 app.Run();
 
--- polling-et-signalr/app/signalr/page.tsx
+++ polling-et-signalr/app/signalr/page.tsx
@@ -4,34 +4,54 @@ import React from "react";
 import { useEffect } from "react";
 import { UselessTask } from "../models/UselessTask";
 import TaskView from "../_components/tasks-view";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 export default function Home() {
 
+  const [hubConnection, setHubConnection] = React.useState<HubConnection>();
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
+  const [usercount, setUserCount] = React.useState<number>(0);
 
   useEffect(() => {
       connecttohub();
     }, []);
 
   function connecttohub() {
-    let testTasks = new Array<UselessTask>(
-          { id: 1, text: "Test Task 1", completed: false },
-          { id: 2, text: "Test Task 2", completed: true });
-        setTasks(testTasks);
     // TODO On doit commencer par créer la connexion vers le Hub
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
     // TODO On doit ensuite se connecter
+
+    let newHubConnection = new HubConnectionBuilder()
+    .withUrl('http://localhost:5042/tasks')
+    .build();
+    // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
+    newHubConnection.on('UserCount', (data) => {
+      setUserCount(data);
+    });
+
+    newHubConnection.on('TaskList', (data) => {
+      setTasks(data);
+    });
+    // TODO On doit ensuite se connecter
+    newHubConnection
+      .start()
+      .then(() => {
+        console.log('La connexion est live!');
+
+      })
+      .catch(err => console.log('Error while starting connection: ' + err))
+
+    setHubConnection(newHubConnection);
   }
 
   function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
-    let tasksCopy : UselessTask[] = [...tasks];    
-    tasksCopy.find(task => task.id === id)!.completed = true;
-    setTasks(tasksCopy);
+    hubConnection!.invoke('CompleteTask', id);
   }
 
-  function handleTaskAdd() {
+  function handleTaskAdd(taskname: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
+    hubConnection!.invoke('AddTask', taskname);
   }
 
   return (
